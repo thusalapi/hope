56a16eff70480ef50810c64c15aeedc869a281d4
Thusala Piyarisi <thusalapi@gmail.com>
2024-10-05 23:19:13 +0530

    add topbar

--- frontend/src/pages/UserManagement.tsx
+++ frontend/src/pages/UserManagement.tsx
@@ -11,6 +11,7 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import ReportButton from "@/components/UserDashboard/ReportButton";
+import TopBar from "@/components/TopBar";
 
 interface User {
   _id?: string;
@@ -42,37 +43,47 @@ const UserManagement: React.FC = () => {
 
   return (
     <div className="flex h-screen">
-      <Sidebar />
+      <div className="fixed top-0 left-0 h-full w-64 bg-gray-800 text-white z-50">
+        <Sidebar />
+      </div>
 
-      <div className="w-3/4 max-container mx-auto mt-20">
-        <div className="flex-1 p-4">
-          <div className="flex items-center justify-between mb-4">
-            <h2 className="text-2xl font-semibold">User List</h2>
-            <Dialog
-              open={isFormOpen}
-              onOpenChange={(open) => {
-                if (!open) handleDialogClose();
-                setIsFormOpen(open);
-              }}
-            >
-              <DialogTrigger asChild>
-                <Button className="bg-blue-500 hover:bg-blue-600 text-white">
-                  Add New User
-                </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>
-                    {selectedUser ? "Edit User" : "Create User"}
-                  </DialogTitle>
-                </DialogHeader>
-                <UserForm user={selectedUser} onFormSubmit={handleFormSubmit} />
-              </DialogContent>
-            </Dialog>
-          </div>
-          <UserList onEdit={handleEditUser} />
+      <div className="w-full ml-48">
+        <div className="relative z-40">
+          <TopBar />
         </div>
-        <ReportButton />
+        <div className="w-3/4 max-container mx-auto mt-20">
+          <div className="flex-1 p-4">
+            <div className="flex items-center justify-between mb-4">
+              <h2 className="text-2xl font-semibold">User List</h2>
+              <Dialog
+                open={isFormOpen}
+                onOpenChange={(open) => {
+                  if (!open) handleDialogClose();
+                  setIsFormOpen(open);
+                }}
+              >
+                <DialogTrigger asChild>
+                  <Button className="bg-blue-500 hover:bg-blue-600 text-white">
+                    Add New User
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>
+                      {selectedUser ? "Edit User" : "Create User"}
+                    </DialogTitle>
+                  </DialogHeader>
+                  <UserForm
+                    user={selectedUser}
+                    onFormSubmit={handleFormSubmit}
+                  />
+                </DialogContent>
+              </Dialog>
+            </div>
+            <UserList onEdit={handleEditUser} />
+          </div>
+          <ReportButton />
+        </div>
       </div>
     </div>
   );
